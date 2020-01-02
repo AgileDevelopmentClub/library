638603f65f13af93efc1b0ef6eeba1943fb267aa
nainaistar <nainaistar@gmail.com>
2020-01-02 13:31:05 +0900

    [Add] 取り急ぎテストが通るようにする。

--- library-frontend-service/tests/unit/example.spec.ts
+++ library-frontend-service/tests/unit/example.spec.ts
@@ -3,7 +3,7 @@ import HelloWorld from '@/components/HelloWorld.vue';
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message';
+    const msg = '読書ランキング(累計)';
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
     });
